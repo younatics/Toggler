dc8001f0a6dd2c9c812105c7aa91ae76ac75bb6a
Seungyoun Yi <younatics@gmail.com>
2017-08-06 20:23:46 +0900

    add sample code

--- TogglerDemo/ViewController.swift
+++ TogglerDemo/ViewController.swift
@@ -66,6 +66,42 @@ class ViewController: UIViewController {
     
     func buttonClicked(_ sender: UIButton) {
         toggler.on(index: sender.tag)
+
+//        switch sender.tag {
+//        case 0:
+//            button1.isSelected = true
+//            button2.isSelected = false
+//            button3.isSelected = false
+//            button4.isSelected = false
+//            button5.isSelected = false
+//        case 1:
+//            button1.isSelected = false
+//            button2.isSelected = true
+//            button3.isSelected = false
+//            button4.isSelected = false
+//            button5.isSelected = false
+//        case 2:
+//            button1.isSelected = false
+//            button2.isSelected = false
+//            button3.isSelected = true
+//            button4.isSelected = false
+//            button5.isSelected = false
+//        case 3:
+//            button1.isSelected = false
+//            button2.isSelected = false
+//            button3.isSelected = false
+//            button4.isSelected = true
+//            button5.isSelected = false
+//        case 4:
+//            button1.isSelected = false
+//            button2.isSelected = false
+//            button3.isSelected = false
+//            button4.isSelected = false
+//            button5.isSelected = true
+//
+//        default:
+//            break
+//        }
     }
 
     func buttonClicked2(_ sender: UIButton) {
