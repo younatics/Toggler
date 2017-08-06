e25e93858edafa1f9f85060afddb78387369015d
Seungyoun Yi <younatics@gmail.com>
2017-08-06 18:15:49 +0900

    add some functions

--- Toggler/Toggler.swift
+++ Toggler/Toggler.swift
@@ -8,6 +8,37 @@
 
 import Foundation
 
-open class Toggler: Any{
+open class Toggler: NSObject {
+    var togglers = [Any]()
+
+    init(default index: Int, togglers: [Any]) {
+        super.init()
+        
+        for toggle in togglers {
+            toggleOff(toggle: toggle)
+        }
+        toggleOn(toggle: togglers[index])
+    }
+    
+    private func toggleOff(toggle: Any) {
+        if let _toggle = toggle as? UIButton {
+            _toggle.isSelected = false
+        } else if let _toggle = toggle as? UIControl {
+            _toggle.isSelected = false
+        } else {
+            fatalError("Not supported type")
+        }
+    }
+    
+    private func toggleOn(toggle: Any) {
+        if let _toggle = toggle as? UIButton {
+            _toggle.isSelected = true
+        } else if let _toggle = toggle as? UIControl {
+            _toggle.isSelected = true
+        } else {
+            fatalError("Not supported type")
+        }
+    }
+
     
 }
--- TogglerDemo/ViewController.swift
+++ TogglerDemo/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Toggler
 
 class ViewController: UIViewController {
 
