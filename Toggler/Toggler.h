47d7267deeaeffd0a7ccae917ec85747bcc476c9
Seungyoun Yi <younatics@gmail.com>
2017-08-06 17:57:55 +0900

    add demo project

---
+++ Toggler/Toggler.h
@@ -0,0 +1,19 @@
+//
+//  Toggler.h
+//  Toggler
+//
+//  Created by YiSeungyoun on 2017. 8. 6..
+//  Copyright © 2017년 SeungyounYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Toggler.
+FOUNDATION_EXPORT double TogglerVersionNumber;
+
+//! Project version string for Toggler.
+FOUNDATION_EXPORT const unsigned char TogglerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Toggler/PublicHeader.h>
+
+
---
+++ Toggler/Toggler.swift
@@ -0,0 +1,13 @@
+//
+//  Toggler.swift
+//  Toggler
+//
+//  Created by YiSeungyoun on 2017. 8. 6..
+//  Copyright © 2017년 SeungyounYi. All rights reserved.
+//
+
+import Foundation
+
+open class Toggler: Any{
+    
+}
